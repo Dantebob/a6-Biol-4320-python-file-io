8462e8462be97d3066c1592667f56bc86dfe57ef
dantebob <dantecelani@gmail.com>
2023-10-10 16:19:59 -0600

    adding regex_ICA.py

--- regex_ICA.py
+++ regex_ICA.py
@@ -18,11 +18,20 @@ def re_search(re_pattern, to_search):
         return "Not found"
 
 result = re_search("for", "Before")
-print(result)
+#print(result)
 
-re_pattern = r'[a-zA-Z]'
-re_pattern_obj = re.compile(re_pattern)
-to_search = 'Canoe'
+#re_pattern = r'[a-zA-Z]'
+#re_pattern_obj = re.compile(re_pattern)
+#to_search = 'Canoe'
 
-x = re_pattern_obj.findall(to_search)
-print(x)
+#x = re_pattern_obj.findall(to_search)
+#print(x)
+if __name__ == '__main__':
+    to_search = "-Hellooo there. 1999 is the year before 2000. yep. hey_you, hello"
+    re_pattern = r'\b(?!t)\w+'
+    re_object = re.compile(re_pattern, re.IGNORECASE)
+    match = re_object.findall(to_search)
+    print(match)
+#    for m in re_pattern_obj.finditer(to_search):
+#        print(m.start, m.end(), m.group(0))
+
